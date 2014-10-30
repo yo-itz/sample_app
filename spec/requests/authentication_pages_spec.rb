c6dc029688659d3af7389b25a8e45d256c9e4c73
Yohei YASUKAWA <yasulab@gmail.com>
2014-10-30 20:08:48 +0900

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,7 +63,6 @@ describe "AuthenticationPages" do
       end
 
       describe "in the Users controller" do
-
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }
@@ -78,20 +77,42 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
     end
 
     describe "in the Microposts controller" do
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
+      describe "submitting to the create action" do
+        before { post microposts_path }
+        specify { expect(response).to redirect_to(signin_path) }
       end
+
+      describe "submitting to the destroy action" do
+        before { delete micropost_path(FactoryGirl.create(:micropost)) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
+
+    describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -123,3 +144,4 @@ describe "AuthenticationPages" do
     end
   end
 end
+
